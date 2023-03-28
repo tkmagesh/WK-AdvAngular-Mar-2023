1c3b1dc44a83ae7a9c922e0af72eb09b64567520
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-28 15:02:59 +0530

    bug tracker - phase - 1

---
+++ first-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  /* 
+    Use Cases:
+      1. Adding bugs
+      2. Listing bugs
+      3. Removing bugs
+      4. Toggling bugs
+      5. Remove closed bugs
+      6. Display Stats
+  */
+}
