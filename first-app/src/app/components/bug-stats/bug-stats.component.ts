bc83cc40c85f3e82b71f807c5eb21616121a6726
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-29 10:08:14 +0530

    rxjs demos

--- first-app/src/app/components/bug-stats/bug-stats.component.ts
+++ first-app/src/app/components/bug-stats/bug-stats.component.ts
@@ -5,7 +5,6 @@ import { Bug } from 'src/app/models/bug';
   selector: 'app-bug-stats',
   templateUrl: './bug-stats.component.html',
   styleUrls: ['./bug-stats.component.css'],
-  changeDetection : ChangeDetectionStrategy.OnPush
 })
 export class BugStatsComponent {
   @Input('data')
